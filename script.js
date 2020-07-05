8e43eabb3a44b733ef337f10c43b618e7ea7fefc
Matt Galan <mattg54321@gmail.com>
2020-07-05 15:02:34 -0700

    added button hover effects

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const rainbow = ["#F94144", "#F3722C", "#F8961E", "#F9C74F", "#90BE6D", "#43AA8B", "#577590"]
+
 function remapCoords(coord) {
     return 50 + (coord - 50) * .5;
 }
@@ -19,6 +21,7 @@ window.onmousemove = event => {
     });
 }
 
-function test() {
-    console.log("test");
+function test(button) {
+    const randomInt = Math.floor(Math.random() * Math.floor(7)) + 1;
+    button.className = "category button-bkg-" + randomInt;
 }
